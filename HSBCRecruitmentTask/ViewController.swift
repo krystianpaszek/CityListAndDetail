d105837ad753298fa0a4f981fbc5e4ed4d03f424
Krystian Paszek <krystian.paszek@offerista.com>
2020-11-14 16:46:13 +0100

    Initial Commit

---
+++ HSBCRecruitmentTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSBCRecruitmentTask
+//
+//  Created by Krystian Paszek on 14/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
