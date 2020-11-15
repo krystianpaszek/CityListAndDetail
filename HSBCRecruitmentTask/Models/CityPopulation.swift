53969d973e16d05fce710a3aecdde92a9f671564
Krystian Paszek <krystian.paszek@offerista.com>
2020-11-15 15:03:52 +0100

    Add CityRating and CityPopulation models to the project

---
+++ HSBCRecruitmentTask/Models/CityPopulation.swift
@@ -0,0 +1,13 @@
+//
+//  CityPopulation.swift
+//  HSBCRecruitmentTask
+//
+//  Created by Krystian Paszek on 15/11/2020.
+//
+
+import Foundation
+
+struct CityPopulation: Decodable {
+    let id: UUID
+    let population: Int
+}
---
+++ HSBCRecruitmentTask/Models/CityRating.swift
@@ -0,0 +1,13 @@
+//
+//  CityRating.swift
+//  HSBCRecruitmentTask
+//
+//  Created by Krystian Paszek on 15/11/2020.
+//
+
+import Foundation
+
+struct CityRating: Decodable {
+    let id: UUID
+    let rating: Float
+}
