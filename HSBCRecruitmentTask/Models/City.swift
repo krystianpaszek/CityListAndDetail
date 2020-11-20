a74b9825f4a9e3416c3508ccc9476bd7eccd1f59
Krystian Paszek <krystian.paszek@offerista.com>
2020-11-20 13:49:41 +0100

    Make City conform to Equatable

--- HSBCRecruitmentTask/Models/City.swift
+++ HSBCRecruitmentTask/Models/City.swift
@@ -8,11 +8,17 @@
 import Foundation
 
 // Models could use their own, strongly typed identifiers
-struct City: Identifiable, Decodable {
+struct City: Identifiable, Equatable, Decodable {
+
     let id: UUID
     let name: String
     let image: URL
 
     var population: CityPopulation?
     var rating: CityRating?
+
+    static func == (lhs: City, rhs: City) -> Bool {
+        return lhs.id == rhs.id
+    }
+
 }
