e1ec0d5cb93ff7fe881e8f7357720b2224c0c698
Krystian Paszek <krystian.paszek@offerista.com>
2020-11-22 14:20:20 +0100

    Implement basic UI tests for first screen

--- HSBCRecruitmentTaskUITests/HSBCRecruitmentTaskUITests.swift
+++ HSBCRecruitmentTaskUITests/HSBCRecruitmentTaskUITests.swift
@@ -9,34 +9,34 @@ import XCTest
 
 class HSBCRecruitmentTaskUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // MARK: - Properties
+    var app: XCUIApplication!
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    // MARK: - Setup
+    override func setUp() {
+        super.setUp()
+
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    // MARK: - Tests
+    func testThatItShowsTableView() {
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let table = app.tables.element
+        XCTAssertTrue(table.exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testThatItHasRefreshButton() {
+        let navigationBar = app.navigationBars["Cities"]
+        let filterButton = navigationBar.buttons["Refresh"]
+        XCTAssertNotNil(filterButton)
+    }
+
+    func testThatItHasFilterButton() {
+        let navigationBar = app.navigationBars["Cities"]
+        let filterButton = navigationBar.buttons["favorite"]
+        XCTAssertNotNil(filterButton.exists)
     }
 }
